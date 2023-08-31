5f0a88d47ddfafb16774ad30d1c6fdf7c8dbc4b9
ArnaudDarre <arnaudarre@gmail.com>
2023-08-31 23:12:43 +0100

    Update storybook manager files

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,36 @@
 /** @type { import('@storybook/react').Preview } */
+import theme from './theme'
+// import './style.scss'
+
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
+    docs: {
+      theme: theme
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/,
       },
     },
+    options: {
+      storySort: {
+        order: [
+          'Intro',
+          'Design system',
+          'Components', [
+            'Utils',
+            'Mixins',
+            'Layout',
+            'Navigation',
+            'Data',
+            'Form',
+            'Interactions'
+          ]
+        ],
+      }
+    }
   },
 };
 
---
+++ .storybook/theme.js
@@ -0,0 +1,32 @@
+import { create } from '@storybook/theming'
+
+export default create({
+  base: 'dark',
+
+  colorPrimary: '#00FFC4',
+  colorSecondary: '#333B4B',
+
+  appBg: '#1A2335',
+  appContentBg: '#000A1E',
+  appBorderColor: 'rgba(255,255,255,0.1)',
+  appBorderRadius: 8,
+
+  fontBase: '"Barlow", sans-serif',
+  fontCode: 'monospace',
+
+  textColor: '#FFFFFF',
+  textInverseColor: '#000A1E',
+
+  barTextColor: '#FFFFFF',
+  barSelectedColor: '#00FFC4',
+  barBg: '#000A1E',
+
+  inputBg: '#1A2335',
+  inputBorder: 'rgba(255,255,255,0.1)',
+  inputTextColor: '#FFFFFF',
+  inputBorderRadius: 8,
+
+  brandTitle: 'Shroomio',
+  brandUrl: '',
+  brandImage: ''
+})
