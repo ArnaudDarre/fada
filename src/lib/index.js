7c746704922357d40d7ab2895849318663f66e8d
ArnaudDarre <arnaudarre@gmail.com>
2023-09-01 15:05:33 +0100

    Add index.js file to package

---
+++ src/lib/index.js
@@ -0,0 +1,97 @@
+import { Accordion } from './components/Accordion'
+import { AccordionItem } from './components/AccordionItem'
+import { Alert } from './components/Alert'
+import { Grid } from './components/Grid'
+import { Breadcrumbs } from './components/Breadcrumbs'
+import { Button } from './components/Button'
+import { Buttons } from './components/Buttons'
+import { Card } from './components/Card'
+import { CardActions } from './components/CardActions'
+import { CardContent } from './components/CardContent'
+import { CardHeader } from './components/CardHeader'
+import { CardMedia } from './components/CardMedia'
+import { Checkbox } from './components/Checkbox'
+import { Chip } from './components/Chip'
+import { Detail } from './components/Detail'
+import { Divider } from './components/Divider'
+import { FormField } from './components/FormField'
+import { Layer } from './components/Layer'
+import { Link } from './components/Link'
+import { Loader } from './components/Loader'
+import { Modal } from './components/Modal'
+import { ModalActions } from './components/ModalActions'
+import { ModalContent } from './components/ModalContent'
+import { ModalHeader } from './components/ModalHeader'
+import { NavBar } from './components/NavBar'
+import { NavGroup } from './components/NavGroup'
+import { NavItem } from './components/NavItem'
+import { Page } from './components/Page'
+import { PageHeader } from './components/PageHeader'
+import { Radio } from './components/Radio'
+import { Section } from './components/Section'
+import { SectionHeader } from './components/SectionHeader'
+import { SelectOption } from './components/SelectOption'
+import { Snippet } from './components/Snippet'
+import { Switch } from './components/Switch'
+import { Table } from './components/Table'
+import { TableBody } from './components/TableBody'
+import { TableCell } from './components/TableCell'
+import { TableFooter } from './components/TableFooter'
+import { TableHead } from './components/TableHead'
+import { TableRow } from './components/TableRow'
+import { Tabs } from './components/Tabs'
+import { TabsContent } from './components/TabsContent'
+import { TabsControl } from './components/TabsControl'
+import { Tag } from './components/Tag'
+import { Text } from './components/Text'
+import { Tooltip } from './components/Tooltip'
+
+export {
+  Accordion,
+  AccordionItem,
+  Alert,
+  Grid,
+  Button,
+  Buttons,
+  Breadcrumbs,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Checkbox,
+  Chip,
+  Detail,
+  Divider,
+  FormField,
+  Layer,
+  Link,
+  Loader,
+  Modal,
+  ModalActions,
+  ModalContent,
+  ModalHeader,
+  NavBar,
+  NavGroup,
+  NavItem,
+  Page,
+  PageHeader,
+  Radio,
+  Section,
+  SectionHeader,
+  SelectOption,
+  Snippet,
+  Switch,
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableRow,
+  Tabs,
+  TabsContent,
+  TabsControl,
+  Tag,
+  Text,
+  Tooltip
+}
